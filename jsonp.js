479a2b5845851d465fb64fa9d06b330cc77a6234
Ivy <ivy@TimdeMacBook-Pro.local>
2017-04-08 22:32:42 +0800

    improve

--- jsonp.js
+++ jsonp.js
@@ -3,12 +3,12 @@
 
 function load_jQuery(fn){
     if(window.jQuery){
-    	fn(); 
+    	fn && fn(); 
     } else { 
 	    var script = document.createElement('script');
    		script.type = 'text/javascript';
-    	script.src = '//cdn.bootcss.com/jquery/3.2.1/jquery.js?callback='+fn.name;
-		script.onload = fn();
+	    	script.src = 'http://cdn.bootcss.com/jquery/3.2.1/jquery.js';
+		script.onload = fn ? fn : null;
     	document.head.appendChild(script);
 	}
 }
